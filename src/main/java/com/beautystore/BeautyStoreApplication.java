43bed3095f118d6723b8f128ed86610f3a904672
Shorobura Halyna <halyna.shorobura@gmail.com>
2019-09-08 15:08:42 +0300

    Initial commit

---
+++ src/main/java/com/beautystore/BeautyStoreApplication.java
@@ -0,0 +1,13 @@
+package com.beautystore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyStoreApplication.class, args);
+    }
+
+}
